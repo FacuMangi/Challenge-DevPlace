c356fca4317afa85f8e814f1ef9ce0c84d7955a3
FacuMangi <facumangioni@gmail.com>
2022-09-22 13:27:21 -0300

    Terminado el challenge 4

---
+++ Modulo Algoritmos/challenge4.py
@@ -0,0 +1,25 @@
+from random import randint
+#Importo de random el metodo randint que devuelve un numero aleatorio dado un determinado rango
+
+def main():
+    randNum = randint(0, 1000)
+    #Se genero el numero
+    print('Se genero un numero entre 0 y 1000. ¿Que numero es?')
+    #Devuelve el mensaje al usuario
+
+    while True:
+        #Se crea el loop que permite intentar todas las veces que el usuaario quiera
+        intento = input('> ')
+        intento = int(intento)
+        
+        if intento == randNum:
+            break
+        if intento < randNum:
+            print('Es mayor')
+        if intento > randNum:
+            print('Es menor')
+        #Si el usuario acierta se cierra el programa, de lo contrario devuelve las pistas dependiendo de la comparacion entre el intento y el numero
+
+if __name__ == '__main__':
+    main()
+#Condicion que llama la funcion main 
