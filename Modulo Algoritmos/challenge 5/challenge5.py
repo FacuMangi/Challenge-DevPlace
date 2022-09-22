c59c89b6f6e704e32677611cdcdd5710de2f6ca1
FacuMangi <facumangioni@gmail.com>
2022-09-22 16:11:22 -0300

    Terminado challenge 5

--- Modulo Algoritmos/challenge 5/challenge5.py
+++ Modulo Algoritmos/challenge 5/challenge5.py
@@ -7,4 +7,9 @@ while True:
     
     numeros.append(numero)
 
-print(numeros)
+numeros.sort()
+print(f"Mayor numero introducido: {numeros[-1]}")
+print(f"Menor numero introducido: {numeros[0]}")
+print(f"Cantidad de numeros introducidos: {len(numeros)}")
+print(f"Suma total de los numeros introducidos: {sum(numeros)}")
+
